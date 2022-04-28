9321b91c45f3bc0f564871d78c912268ca84a4ec
Dino Rodriguez <dinoorodriguez@gmail.com>
2022-04-28 12:58:41 -0400

    enable medium and low

--- contracts/GDA.sol
+++ contracts/GDA.sol
@@ -119,6 +119,9 @@ contract GDA is ERC721A {
     function _getCurrentStepAndPrice() internal view returns (uint256, uint256) {
         uint256 step = _getStep();
 
+        // False positive guarding against using strict equality checks
+        // Shouldn't be a problem here because we check for > and < as well 
+        // slither-disable-next-line incorrect-equality
         if (step == _currentStep) {
             return (_currentStep, _pricePerStep[_currentStep]);
         } else if (step > _currentStep) {
